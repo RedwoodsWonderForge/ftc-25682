44a2b1361452375792597fb80ab9f811f143e88a
WonderForgeR <robert@redwoodswonderforge.org>
2025-11-25 16:25:08 -0800

    working on LeaveGoal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueGoal.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "BlueGoal")
+public class BlueGoal extends LinearOpMode {
+    LeaveGoal leaveGoal;
+    private DcMotor FL_MOTOR;
+    private DcMotor FR_MOTOR;
+    private DcMotor BL_MOTOR;
+    private DcMotor BR_MOTOR;
+
+    /**
+     * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
+     * Comment Blocks show where to place Initialization code (runs once, after touching the
+     * DS INIT button, and before touching the DS Start arrow), Run code (runs once, after
+     * touching Start), and Loop code (runs repeatedly while the OpMode is active, namely not
+     * Stopped).
+     */
+    @Override
+    public void runOpMode() {
+        FL_MOTOR = hardwareMap.get(DcMotor.class, "FL_MOTOR");
+        FR_MOTOR = hardwareMap.get(DcMotor.class, "FR_MOTOR");
+        BL_MOTOR = hardwareMap.get(DcMotor.class, "BL_MOTOR");
+        BR_MOTOR = hardwareMap.get(DcMotor.class, "BR_MOTOR");
+        String direction = "cw";
+        leaveGoal = new LeaveGoal(FL_MOTOR, FR_MOTOR, BL_MOTOR, BR_MOTOR, direction);
+        leaveGoal.initalSetup();
+        waitForStart();
+        leaveGoal.autoDrive();
+    }
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeaveGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeaveGoal.java
@@ -1,43 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "RedGoal")
-public class RedGoal extends LinearOpMode {
-    private DcMotor FL_MOTOR;
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class LeaveGoal{
+
+
+    private DcMotor FlMotor;
     private DcMotor FR_MOTOR;
     private DcMotor BL_MOTOR;
     private DcMotor BR_MOTOR;
     double turn;
-    double forward;
+    private double forward;
     double maxDrivePower;
     double strafe;
+    private String Direction;
 
-    /**
-     * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
-     * Comment Blocks show where to place Initialization code (runs once, after touching the
-     * DS INIT button, and before touching the DS Start arrow), Run code (runs once, after
-     * touching Start), and Loop code (runs repeatedly while the OpMode is active, namely not
-     * Stopped).
-     */
-    @Override
-    public void runOpMode() {
-        FL_MOTOR = hardwareMap.get(DcMotor.class, "FL_MOTOR");
-        FR_MOTOR = hardwareMap.get(DcMotor.class, "FR_MOTOR");
-        BL_MOTOR = hardwareMap.get(DcMotor.class, "BL_MOTOR");
-        BR_MOTOR = hardwareMap.get(DcMotor.class, "BR_MOTOR");
-
-        initalSetup();
-        waitForStart();
-        autoDrive();
+    public LeaveGoal(DcMotor flMotor, DcMotor frMotor, DcMotor blMotor, DcMotor brMotor, String direction) {
+        FlMotor = flMotor;
+        FR_MOTOR = frMotor;
+        BL_MOTOR = blMotor;
+        BR_MOTOR = brMotor;
+        Direction = direction;
     }
 
     /**
      * Describe this function...
      */
-    private void initalSetup() {
+    public void initalSetup() {
         maxDrivePower = 1;
         turn = 0;
         forward = 0;
@@ -49,7 +38,7 @@ public class RedGoal extends LinearOpMode {
     /**
      * Describe this function...
      */
-    private void autoDrive() {
+    public void autoDrive() {
         backward();
         turn();
         backward();
@@ -77,7 +66,7 @@ public class RedGoal extends LinearOpMode {
         forward = forward * maxDrivePower;
         strafe = strafe * maxDrivePower;
         // Combine inputs to create drive and turn (or both)
-        FL_MOTOR.setPower(forward + turn + strafe);
+        FlMotor.setPower(forward + turn + strafe);
         FR_MOTOR.setPower((forward - turn) - strafe);
         BL_MOTOR.setPower((forward + turn) - strafe);
         BR_MOTOR.setPower((forward - turn) + strafe);
@@ -92,8 +81,20 @@ public class RedGoal extends LinearOpMode {
         prosessInputsAndSleep(400);
     }
     private void turn() {
-        turn = -1;
+        if (Direction == "CCW") {
+            turn = -1;
+        } else {
+            turn = 1;
+        }
+
         prosessInputsAndSleep(280);
     }
 
+    public void sleep(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds); // Pause for 'milliseconds'
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restore the interrupted status
+        }
+    }
 }
