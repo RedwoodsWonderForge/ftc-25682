6f03ca006e41cc4eafd54b4a0037b4f106d5a53c
WonderForgeR <robert@redwoodswonderforge.org>
2025-11-25 17:11:06 -0800

    Apply LeaveGoal to RedGoal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedGoal.java
@@ -6,14 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "RedGoal")
 public class RedGoal extends LinearOpMode {
+    LeaveGoal leaveGoal;
     private DcMotor FL_MOTOR;
     private DcMotor FR_MOTOR;
     private DcMotor BL_MOTOR;
     private DcMotor BR_MOTOR;
-    double turn;
-    double forward;
-    double maxDrivePower;
-    double strafe;
 
     /**
      * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
@@ -29,71 +26,12 @@ public class RedGoal extends LinearOpMode {
         BL_MOTOR = hardwareMap.get(DcMotor.class, "BL_MOTOR");
         BR_MOTOR = hardwareMap.get(DcMotor.class, "BR_MOTOR");
 
-        initalSetup();
+        String direction = "ccw";
+        leaveGoal = new LeaveGoal(FL_MOTOR, FR_MOTOR, BL_MOTOR, BR_MOTOR, direction);
+        leaveGoal.initalSetup();
         waitForStart();
-        autoDrive();
+        leaveGoal.autoDrive();
     }
 
-    /**
-     * Describe this function...
-     */
-    private void initalSetup() {
-        maxDrivePower = 1;
-        turn = 0;
-        forward = 0;
-        strafe = 0;
-        // "really weird" -Misty
-
-    }
-
-    /**
-     * Describe this function...
-     */
-    private void autoDrive() {
-        backward();
-        turn();
-        backward();
-    }
-
-    /**
-     * Describe this function...
-     */
-    private void prosessInputsAndSleep(int duration) {
-        // "This makes the code cleaner"??
-        processDriveInputs();
-        sleep(duration);
-        // Stop all movement after sleep
-        turn = 0;
-        forward = 0;
-        strafe = 0;
-        processDriveInputs();
-    }
-
-    /**
-     * callculates BL BR FL FR motors (Btw. Does not stop)
-     */
-    private void processDriveInputs() {
-        turn = turn * maxDrivePower;
-        forward = forward * maxDrivePower;
-        strafe = strafe * maxDrivePower;
-        // Combine inputs to create drive and turn (or both)
-        FL_MOTOR.setPower(forward + turn + strafe);
-        FR_MOTOR.setPower((forward - turn) - strafe);
-        BL_MOTOR.setPower((forward + turn) - strafe);
-        BR_MOTOR.setPower((forward - turn) + strafe);
-    }
-
-
-    /**
-     * Describe this function...
-     */
-    private void backward() {
-        forward = -1;
-        prosessInputsAndSleep(400);
-    }
-    private void turn() {
-        turn = -1;
-        prosessInputsAndSleep(280);
-    }
 
 }
